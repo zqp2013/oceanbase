97dc701712d1bb0f995a35a8866ba20c98659706
zqp2013 <zqp2013@126.com>
2019-12-15 00:24:36 +0800

    Update automake.sh

--- oceanbase_0.4/automake.sh
+++ oceanbase_0.4/automake.sh
@@ -4,3 +4,6 @@ aclocal
 #根据configure.ac及aclocal.m4生成configuration
 autoconf
 ./build.sh
+
+
+#One also needs to run 'autoreconf -i' before rerunning ./configure - it will install the missing files that ./configure is complaining about.
