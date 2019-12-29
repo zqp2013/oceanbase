c8ce8cfb1a80f85a9e401d305398831e87f2d8b4
zqp2013 <zqp2013@126.com>
2019-12-29 15:29:32 +0800

    Update auto_make.sh

--- oceanbase_0.4/auto_make.sh
+++ oceanbase_0.4/auto_make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export DRC_ROOT=/usr/bin/
-export TBLIB_ROOT=/home/qpzhou/sources/tb-common-utils/
+export TBLIB_ROOT=/home/qpzhou/sources/tair/deps/tbsys-install/  #/home/qpzhou/sources/tb-common-utils/
 export EASY_ROOT=/usr/local/   # tail depends libeasy :make install
 
 libtoolize --force
